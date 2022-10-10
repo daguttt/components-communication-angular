ebf412d27e24a804efba4202c5f55a9964310a9f
daguttt <70865733+daguttt@users.noreply.github.com>
2022-10-10 18:41:57 -0500

    add first components and basic markup. UNFINISHED

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
 
-import { HelloComponent } from "./hello.component";
+import { HelloComponent } from './hello.component';
+import { ParentComponent } from './components/parent/parent.component';
+import { ChildComponent } from './components/child/child.component';
+import { GrandChildComponent } from './components/grand-child/grand-child.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    ParentComponent,
+    ChildComponent,
+    GrandChildComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
