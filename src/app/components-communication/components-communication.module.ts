79f8106dadb2957d9f63146e977700cec258f778
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-10-10 17:09:28 -0500

    fix: show `app-parent` into ParentChildComponent page

--- src/app/components-communication/components-communication.module.ts
+++ src/app/components-communication/components-communication.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { ComponentsCommunicationModuleRouting } from './components-communication-routing.module';
+import { ComponentsCommunicationModuleRouting } from "./components-communication-routing.module";
 
-import { ParentComponent } from './components/parent/parent.component';
-import { ChildComponent } from './components/child/child.component';
-import { GrandChildComponent } from './components/grand-child/grand-child.component';
-import { ComponentsCommunicationComponent } from './components-communication.component';
-import { ParentChildComponent } from './pages/parent-child/parent-child.component';
-import { ChildParentComponent } from './pages/child-parent/child-parent.component';
-import { ParentChildGrandChildComponent } from './pages/parent-child-grand-child/parent-child-grand-child.component';
-import { GrandChildChildParentComponent } from './pages/grand-child-child-parent/grand-child-child-parent.component';
+import { ParentComponent } from "./components/parent/parent.component";
+import { ChildComponent } from "./components/child/child.component";
+import { GrandChildComponent } from "./components/grand-child/grand-child.component";
+import { ComponentsCommunicationComponent } from "./components-communication.component";
+import { ParentChildComponent } from "./pages/parent-child/parent-child.component";
+import { ChildParentComponent } from "./pages/child-parent/child-parent.component";
+import { ParentChildGrandChildComponent } from "./pages/parent-child-grand-child/parent-child-grand-child.component";
+import { GrandChildChildParentComponent } from "./pages/grand-child-child-parent/grand-child-child-parent.component";
 
 @NgModule({
   imports: [CommonModule, ComponentsCommunicationModuleRouting],
@@ -19,10 +19,10 @@ import { GrandChildChildParentComponent } from './pages/grand-child-child-parent
     ParentComponent,
     ChildComponent,
     GrandChildComponent,
-    // ParentChildComponent,
-    // ChildParentComponent,
-    // ParentChildGrandChildComponent,
-    // GrandChildChildParentComponent,
+    ParentChildComponent,
+    ChildParentComponent,
+    ParentChildGrandChildComponent,
+    GrandChildChildParentComponent,
   ],
 })
 export class ComponentsCommunicationModule {}
