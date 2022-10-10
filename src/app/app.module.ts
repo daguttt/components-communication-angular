aa843cd3c0ed0fa036aa97342ac215d153953bea
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-10-10 18:13:17 -0500

    refactor: reset project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
-import { ComponentsCommunicationModule } from './components-communication/components-communication.module';
-
-import { HelloComponent } from './hello.component';
-import { AppRoutingModule } from './app-routing.module';
+import { HelloComponent } from "./hello.component";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    ComponentsCommunicationModule,
-  ],
+  imports: [BrowserModule, FormsModule],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
